0dc16ce1f1b3c242983b4de915fe19994a8296bd
Ali Dbouk <alihassandbouk1@gmail.com>
2025-04-04 23:29:41 +0300

    Add something

--- react-chest-xray/src/App.js
+++ react-chest-xray/src/App.js
@@ -33,7 +33,7 @@ function App() {
       setResult('');
 
       // Adjust to match your Flask server endpoint & port
-      const response = await axios.post('http://127.0.0.1:5000/predict', formData, {
+      const response = await axios.post('http://127.0.0.1:5555/predict', formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
